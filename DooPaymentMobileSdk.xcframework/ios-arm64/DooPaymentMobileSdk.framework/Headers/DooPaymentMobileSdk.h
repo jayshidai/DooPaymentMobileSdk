f56190837f1e103e9e87f0c41f90b2861164f06b
Novice.Cai <novice@finovy.tech>
2025-03-25 15:03:38 +0800

    init codes

---
+++ DooPaymentMobileSdk.xcframework/ios-arm64/DooPaymentMobileSdk.framework/Headers/DooPaymentMobileSdk.h
@@ -0,0 +1,18 @@
+//
+//  DooPaymentMobileSdk.h
+//  DooPaymentMobileSdk
+//
+//  Created by cailei on 2025/3/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DooPaymentMobileSdk.
+FOUNDATION_EXPORT double DooPaymentMobileSdkVersionNumber;
+
+//! Project version string for DooPaymentMobileSdk.
+FOUNDATION_EXPORT const unsigned char DooPaymentMobileSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DooPaymentMobileSdk/PublicHeader.h>
+
+
